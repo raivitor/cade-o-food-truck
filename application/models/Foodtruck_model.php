f273e86a79b20976c5ac8e21af806edf30102a7c
Rai Vitor <raivitor@gmail.com>
2016-06-14 16:27:43 -0300

    finalizando empresa model

--- application/models/Foodtruck_model.php
+++ application/models/Foodtruck_model.php
@@ -14,10 +14,9 @@ class Foodtruck_model extends CI_Model {
     function getTruck($id) {
         if($id) {
             $query = $this->db->query("SELECT * FROM foodtruck WHERE id='id'");
-            foreach($query->result() as $row) {
-              $foodtrucks[] = $row;
+            if($query->num_rows() == 1) {
+                return $query->result();
             }
-            return $foodtrucks;
         }
     }
 
@@ -32,22 +31,23 @@ class Foodtruck_model extends CI_Model {
     }
 
     function getTruckEmpresa($idEmpresa) {
-        if($idEmpresa) {
-            $query = $this->db->query("SELECT * FROM foodtruck WHERE empresa_id='idEmpresa'");
-            foreach($query->result() as $row) {
-              $foodtrucks[] = $row;
-            }
-            return $foodtrucks;
-        }
+      if($idEmpresa) {
+          $query = $this->db->query("SELECT * FROM foodtruck WHERE empresa_id='idEmpresa'");
+          foreach($query->result() as $row) {
+            $foodtrucks[] = $row;
+          }
+          return $foodtrucks;
+      }
     }    
 
     function createTruck($new_emp) {
+       $nome        = $new_truck['nome'];
+       $descricao   = $new_truck['descricao'];
+       $telefone    = $new_truck['telefone'];
+       $logo        = $new_truck['logo'];
+       $fotos       = $new_truck['fotos'];
 
-       $nome  = $new_emp['name'];
-       $cnpj  = $new_emp['cnpj'];
-       $idDono = $new_emp['idDono'];
-
-       $query = "INSERT INTO foodtruck (nome, cnpj, idDono) VALUES ('$nome', '$cnpj', '$idDono')";
+       $query = "INSERT INTO foodtruck (nome, descricao, telefone, logo, fotos) VALUES ('$nome', '$descricao', '$telefone', '$logo', '$fotos')";
 
        if(!$this->db->query($query)){
           return $this->db->error();
@@ -56,14 +56,17 @@ class Foodtruck_model extends CI_Model {
         }
     }
 
-    function updateTruck($emp) {
-       $id    = $emp['id'];
-       $nome  = $emp['nome'];
-       $cnpj  = $emp['cnpj'];
+    function updateTruck($truck) {
+       $id          = $truck['id'];
+       $nome        = $truck['nome'];
+       $descricao   = $truck['descricao'];
+       $telefone    = $truck['telefone'];
+       $logo        = $truck['logo'];
+       $fotos       = $truck['fotos'];
 
-       $query = "UPDATE foodtruck SET nome='$nome', cnpj='$cnpj' WHERE id='$id'"; 
+       $query = "UPDATE foodtruck SET nome = '$nome', descricao = '$descricao', telefone = '$telefone', logo = '$logo', fotos = '$fotos' WHERE id='$id'"; 
 
-       if( ! $this->db->query($query)) {
+       if(!$this->db->query($query)) {
             return $this->db->error();
         } else {
             return true;
@@ -78,8 +81,14 @@ class Foodtruck_model extends CI_Model {
         }
     }
 
-    function assocGerente($idGerente){
-
+    function assocGerente($idGerente, $idTruck){
+      $query = "UPDATE foodtruck SET gerente_id = '$idGerente' where id = '$idTruck'";
+      
+        if(!$this->db->query($query)) {
+            return $this->db->error();
+        } else {
+            return true;
+        }
     }
 
 }
